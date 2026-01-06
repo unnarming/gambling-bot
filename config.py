92a67e6d45a5612df425cddad3bf80d859a6f1cb
unnarming <unnarming@gmail.com>
2026-01-06 08:44:44 -0500

    Add print statement for ENABLE_DMS variable

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
         self.MAX_LOSS_STREAK = int(os.getenv('MAX_LOSS_STREAK'))
         self.STREAK_BIAS = float(os.getenv('STREAK_BIAS'))
         self.BASE_ODDS = int(os.getenv('BASE_ODDS'))
+        print(self.ENABLE_DMS)
 
         self.permission_whitelist_uids: list[int] = [1209228744921710664]
         self.bot_channel: int = int(os.getenv('BOT_CHANNEL'))
